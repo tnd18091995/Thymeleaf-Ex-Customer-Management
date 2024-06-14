0c6153a859db297f198c31b044cb551750e19d0a
tnd18091995 <tnd18091995@gmail.com>
2024-06-14 09:31:02 +0700

    doneappconfiguration

--- src/main/java/service/CustomerService.java
+++ src/main/java/service/CustomerService.java
@@ -1,2 +1,45 @@
-package service;public class CustomerService {
+package service;
+
+import model.Customer;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class CustomerService implements ICustomerService{
+    private static final Map<Integer, Customer> customers;
+    static {
+        customers = new HashMap<>();
+        customers.put(1, new Customer(1,"John", "john@gmail.com", "Ha Noi"));
+        customers.put(2, new Customer(2,"John", "john@gmail.com", "Ha Noi"));
+        customers.put(3, new Customer(3,"John", "john@gmail.com", "Ha Noi"));
+        customers.put(4, new Customer(4,"John", "john@gmail.com", "Ha Noi"));
+        customers.put(5, new Customer(5,"John", "john@gmail.com", "Ha Noi"));
+        customers.put(6, new Customer(6,"John", "john@gmail.com", "Ha Noi"));
+    }
+    @Override
+    public List<Customer> findAll() {
+        return new ArrayList<>(customers.values());
+    }
+
+    @Override
+    public void save(Customer customer) {
+        customers.put(customer.getId(), customer);
+    }
+
+    @Override
+    public Customer findById(int id) {
+        return customers.get(id);
+    }
+
+    @Override
+    public void update(int id, Customer customer) {
+        customers.put(id, customer);
+    }
+
+    @Override
+    public void remove(int id) {
+        customers.remove(id);
+    }
 }
